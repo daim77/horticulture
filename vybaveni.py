482e63576d1f58e5c66269352d9ca595a93c405c
„Martin <martindanek4@gmail.com>
2020-12-08 21:51:58 +0100

    kostra hotova

--- vybaveni.py
+++ vybaveni.py
@@ -1,5 +1,5 @@
 # generuje seznam nutneho vybaveni na zvolene cinnosti
-senam_cinnosti = {
+seznam_cinnosti = {
     1: (
         'kul', 'tlouk', 'palice', 'hnojivo', 'pilka', 'pricka', 'hrebik_60_mm', 'hrebikovacka', 'uvazy', 'ryc',
         'krumpac', 'obaleni_kmenu', 'nuzky', 'nuz', 'strom', 'nahradni_kul', 'nahradni_pricka', 'lopata_srdcovka',
@@ -10,18 +10,29 @@ senam_cinnosti = {
         'nuz', 'trvalka', 'konev', 'mulc', 'pasmo', 'spej'
     ),
     3: (
-        'provazek', 'sprej', 'palice', 'metr', 'pasmo', 'koliky', 'kolecko_kovove', 'vodovaha_velka', 'vodovaha_mala',
+        'provazek', 'sprej', 'palice', 'metr', 'pasmo', 'roxor', 'kolecko_kovove', 'vodovaha_velka', 'vodovaha_mala',
         'ryc', 'krumpac', 'nuz', 'strom', 'nahradni_kul', 'lopata_srdcovka', 'hrabe_svedske', 'hrabe_kovove', 'koste'
     ),
-    4: 'stavba_zdi',
+    4: (
+        'stavba_zdi', 0
+    ),
     5: (
         'valec', 'hrabe_kovove', 'hrabe_svedske', 'kolecko_kovove', 'semeno', 'pasmo', 'sprej'
     ),
-    6: 'koseni_travniku',
-    7: 'udrzba_zahonu',
-    8: 'strihani_stromu',
-    9: 'ostatni'
+    6: (
+        'koseni_travniku', 0
+    ),
+    7: (
+        'udrzba_zahonu', 0
+    ),
+    8: (
+        'strihani_stromu', 0
+    ),
+    9: (
+        'mulcovani', 'klecko_plastove', 'hrabe_kovove', 'lopata_normalni', 'hrabe_svedske', 'koste'
+    )
 }
+kompletni_seznam = set()
 # seznamy materialu
 strihani_stromu = {}
 ostatni = {}
@@ -39,7 +50,7 @@ print(
         6    |    koseni travniku
         7    |    udrzba zahonu
         8    |    strihani stromu
-        9    |    ostatni
+        9    |    mulcovani
     '''
 )
 print('=' * 20)
@@ -48,8 +59,15 @@ print('=' * 20)
 if not volba.isdigit():
     exit()
 volba_list = list(volba)
-for i in range(len(volba)):
-    kompletni_seznam = set(seznam_cinnosti[int(volba_list.pop())])
-    print(kompletni_seznam)
-# OUTPUT
 
+# generovani kompletniho seznamu
+for i in range(len(volba)):
+    volbapop = int(volba_list.pop())
+    print(volbapop)
+    kompletni_seznam = kompletni_seznam | set(seznam_cinnosti[int(volbapop)])
+
+# OUTPUT
+print('=' * 20)
+kompletni_seznam = list(kompletni_seznam)
+while kompletni_seznam:
+    print(kompletni_seznam.pop())
