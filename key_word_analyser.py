f5fb4b34ba93de8f0f188acbef4360b65f6322c5
„Martin <martindanek4@gmail.com>
2021-01-05 21:29:35 +0100

    stat

---
+++ key_word_analyser.py
@@ -0,0 +1,35 @@
+# =====  Analysis key words for hortycurtularist =====
+with open('/Users/martindanek/Documents/na_luka_cz/all_text.txt') as all_web_text:
+    TEXTS = all_web_text.read()
+
+# definitions for variables
+word_count = {}
+numerosity_count = {}
+
+# text clean up
+text_list = TEXTS.split()
+for num, char in enumerate(text_list):
+    word = char.strip('(©),.:+/;\\-@1234567890')
+    if len(word) < 4:
+        text_list[num] = ''
+        continue
+    text_list[num] = word.lower()
+while '' in text_list:
+    text_list.remove('')
+
+# stats for text
+# words count total
+for char in text_list:
+    word_count[char] = word_count.get(char, 1) + 1
+
+# count numerosity of the words
+for numerosity in sorted(list(set(word_count.values()))):
+    numerosity_count[numerosity] = []
+    for word in word_count:
+        numerosity_count[numerosity].append(word)
+
+
+
+# FINAL OUTPUT
+from pprint import pprint as pp
+pp(numerosity_count)
