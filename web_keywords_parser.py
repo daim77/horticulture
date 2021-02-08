70d840193a11987cc3a5730aa1e31293bc8b8ac3
„Martin <martindanek4@gmail.com>
2021-02-08 16:59:26 +0100

    tags

--- web_keywords_parser.py
+++ web_keywords_parser.py
@@ -1,10 +1,6 @@
-# TODO vstupem funkce bude webova adresa, na ni se hledaji nasledujici tagy:
-# TODO title, alt-img, H1, H2, links, strong, em, keywords argument
-# TODO vystupem je txt soubor se slovama
 # TODO nove href pouzije znovu pro hledani
 # TODO maximalni opakovani parseru je pro 100 odkazu
 
-import csv
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -17,60 +13,70 @@ def boilsoup(url):
 
 
 def find_title(file_name, soup):
-    result = soup.find('title').text
-    print(result)
-    csv_writer(file_name, result)
+    title_result = soup.find_all('title')
+    result = [item.text for item in title_result]
+    txt_writer(file_name, result)
     return
 
 
-def find_h1(soup):
-    pass
+def find_h1(file_name, soup):
+    h1_result = soup.find_all('h1')
+    result = [item.text for item in h1_result]
+    txt_writer(file_name, result)
+    return
 
 
-def find_h2(soup):
-    pass
+def find_h2(file_name, soup):
+    h2_result = soup.find_all('h2')
+    result = [item.text for item in h2_result]
+    txt_writer(file_name, result)
+    return
 
 
-def find_strong(soup):
-    pass
+def find_strong(file_name, soup):
+    strong_result = soup.find_all('strong')
+    result = [item.text for item in strong_result]
+    txt_writer(file_name, result)
+    return
 
 
-def find_em(soup):
-    pass
+def find_em(file_name, soup):
+    em_result = soup.find_all('em')
+    result = [item.text for item in em_result]
+    txt_writer(file_name, result)
+    return
 
 
 def find_alt_img(soup):
     pass
 
 
-def find_keywords(soup):
-    pass
+def find_keywords(file_name, soup):
+    try:
+        meta_result = soup.find(attrs={'name': 'keywords'})
+        result = list(meta_result['content'])
+        txt_writer(file_name, result)
+    except:
+        return
+    return
 
 
 def find_href(soup):
     pass
 
 
-def csv_writer(file_name, result):
+def txt_writer(file_name, result):
     path = os.getcwd() + os.sep + 'tables'
     if not os.path.exists(path):
         os.mkdir(path)
 
-    file_name += '.csv'
+    file_name += '.txt'
 
     try:
-        with open(path + os.sep + file_name, mode='a', encoding='utf-8') \
-                as csv_file:
-
-            writer = csv.writer(csv_file, delimiter=',')
-
-            for row in result:
-                writer.writerow(row)
-
-            print('You are lucky! Data available here:')
-            print('=' * 100)
-            print(path + os.sep + file_name)
-            print('=' * 100)
+        with open(path + os.sep + file_name, mode='w', encoding='utf-8') \
+                as txt_file:
+            for item in result:
+                txt_file.writelines(' ' + item)
     except IOError:
         print("I/O error")
     return
@@ -79,8 +85,14 @@ def csv_writer(file_name, result):
 def main(file_name, url):
     soup = boilsoup(url)
     find_title(file_name, soup)
-    pass
+    find_h1(file_name, soup)
+    find_h2(file_name, soup)
+    find_strong(file_name, soup)
+    find_em(file_name, soup)
+
+    find_keywords(file_name, soup)
 
 
 if __name__ == '__main__':
     main('na_luka', 'https://www.naluka.cz')
+    main('pereny_org', 'https://pereny.org')
